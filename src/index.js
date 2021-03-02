7aca75407f073f897bf9410761012e0bffc1bd0d
DLama <yeouvi29@gmail.com>
2021-03-01 19:36:41 -0800

    JSX Code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1> My Wish List </h1>
+    <ul>
+      <li> Tokbokki </li>
+      <li> Hamburger </li>
+      <li> Tangsuyuk </li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
 //Create a react app from scratch.
 //It should display a h1 heading.
 //It should display an unordered list (bullet points).
